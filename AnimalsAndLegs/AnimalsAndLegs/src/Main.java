01400dab503c354f4d80b1ba5ae3886a81c6a664
MRicsi1211 <miklosrichard2002@gmail.com>
2023-09-14 11:04:54 +0200

    Kész

--- AnimalsAndLegs/AnimalsAndLegs/src/Main.java
+++ AnimalsAndLegs/AnimalsAndLegs/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Add meg a csirkék számát: ");
+        int csirke = scanner.nextInt();
+
+        System.out.print("Add meg a malacok számáz: ");
+        int malac = scanner.nextInt();
+
+
+        int ossz = (csirke * 2) + (malac * 4);
+
+
+        System.out.println("Total number of legs for all animals: " + ossz);
+
+
+        scanner.close();
 
         
     }
